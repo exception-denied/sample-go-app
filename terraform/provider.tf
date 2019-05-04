61822c15e13827de1b3bd85e6c79cd94412c74c1
krishna <krishna.201360046@nist.edu>
2019-05-05 02:26:36 +0530

    modifed provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                  = "${var.aws_region}"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
+#  region                  = "${var.aws_region}"
+ # shared_credentials_file = "~/.aws/credentials"
+ # profile                 = "default"
 }
